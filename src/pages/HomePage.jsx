96e2c0d8a8a022beccaa02ec2396c3b3e73a7750
Walter Bernal <wltrbm@gmail.com>
2023-08-24 06:14:00 -0700

    added test for hardcoded API request

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
+import { Button } from "@mui/material"
+import traineeService from "../services/trainee.service"
+
+const handleOnClick = async () => {
+  const respose = await traineeService.getTrainee()
+  console.log(respose)
+}
+
 function HomePage() {
   return (
     <div>
       HomePage
+      <Button onClick={handleOnClick}>Get Trainees</Button>
     </div>
   )
 }
