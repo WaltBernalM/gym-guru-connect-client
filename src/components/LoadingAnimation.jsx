d20fdb3c5b0afb3644f071fe5831ab590c984743
Walter Bernal <wltrbm@gmail.com>
2023-08-30 23:42:48 -0700

    Component creation for loading

---
+++ src/components/LoadingAnimation.jsx
@@ -0,0 +1,27 @@
+import Lottie from "lottie-web"
+import animationData from '../utils/animation_llyrilz5.json'
+import { useRef, useEffect } from "react"
+
+function LoadingAnimation() {
+  const animationContainer = useRef(null)
+  let animation
+
+  useEffect (() => {
+    animation = Lottie.loadAnimation({
+      container: animationContainer.current,
+      animationData,
+      loop: true,
+      autoplay: true
+    })
+
+    return () => {
+      animation.destroy()
+    }
+  }, [])
+
+  return (
+    <div ref={animationContainer} style={{width: '40%'}}/>
+  )
+}
+
+export default LoadingAnimation
