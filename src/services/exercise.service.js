163a7a517ae406a92c1157f3b365cd22e9735947
Walter Bernal <wltrbm@gmail.com>
2023-09-07 03:32:54 -0700

    skull to get info of exercises

--- src/services/exercise.service.js
+++ src/services/exercise.service.js
@@ -1,7 +1,9 @@
 import service from "./index"
 
 const exerciseService = {
-  getAllExercises: () => service.get('/api/exercises')
+  getAllExercises: () => service.get('/api/exercises'),
+  getExercise: () => service.get(/),
+  updateExerciseData: (exerciseId, traineeId, ) => service.put(/)
 }
 
 export default exerciseService
