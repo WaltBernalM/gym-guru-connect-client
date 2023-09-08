5a1ff82a37fede91561d6148dc3295f28cc6a1dc
Walter Bernal <wltrbm@gmail.com>
2023-09-08 03:19:48 -0700

    setup of services to handle exercise features

--- src/services/exercise.service.js
+++ src/services/exercise.service.js
@@ -1,9 +1,33 @@
 import service from "./index"
 
 const exerciseService = {
-  getAllExercises: () => service.get('/api/exercises'),
-  getExercise: () => service.get(/),
-  updateExerciseData: (exerciseId, traineeId, ) => service.put(/)
+  getAllExercises: () => service.get("/api/exercises"),
+  postCustomExerciseToRoutine: (
+    exerciseId,
+    traineeId,
+    exerciseRoutineId,
+    series,
+    reps,
+    intensity
+  ) => {
+    return service.post(`/api/exercises/${exerciseId}/trainee/${traineeId}`, {
+      exerciseRoutineId,
+      series,
+      reps,
+      intensity,
+    })
+  },
+  getExerciseInfo: (exerciseId, traineeId) =>{
+    return service.get(`/api/exercises/${exerciseId}/trainee/${traineeId}`)
+  },
+  updateExerciseData: (exerciseId, traineeId, series, reps, intensity) =>
+    service.put(`/api/exercises/${exerciseId}/trainee/${traineeId}`, {
+      series,
+      reps,
+      intensity,
+    }),
+  deleteExercise: (exerciseId, traineeId) =>
+    service.delete(`/api/exercises/${exerciseId}/trainee/${traineeId}`),
 }
 
 export default exerciseService
